a135f7a795b5d98069233d311ed5634a513d036b
Maxime Deboffle <maxime.deboffle@hotmail.com>
2020-05-20 20:50:49 +0200

    Removing Receiver.java

--- src/main/java/com/esgi/rabbitmq/RabbitmqApplication.java
+++ src/main/java/com/esgi/rabbitmq/RabbitmqApplication.java
@@ -1,17 +1,7 @@
 package com.esgi.rabbitmq;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
-import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RabbitmqApplication {
@@ -19,5 +9,4 @@ public class RabbitmqApplication {
 	public static void main(String[] args){
 		SpringApplication.run(RabbitmqApplication.class, args);
 	}
-
 }
