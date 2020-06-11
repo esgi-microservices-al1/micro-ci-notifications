8892b7c068c611b37da98d2e2b586950a7caa7ba
Maxime Deboffle <maxime.deboffle@hotmail.com>
2020-06-11 12:09:03 +0200

    Mailing ok

--- apiMail/src/main/java/com/esgi/apimail/services/MailSender.java
+++ apiMail/src/main/java/com/esgi/apimail/services/MailSender.java
@@ -2,14 +2,45 @@ package com.esgi.apimail.services;
 
 import com.esgi.apimail.domain.models.Message;
 import com.esgi.apimail.domain.sendmail.contracts.IMailSender;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMailMessage;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 import java.util.Collection;
 
 @Service
 public class MailSender implements IMailSender {
+    @Value("${mail.address}")
+    private String senderMailAddress;
+
+    public final JavaMailSender emailSender;
+
+    public MailSender(JavaMailSender emailSender) {
+        this.emailSender = emailSender;
+    }
+
     @Override
     public boolean sendMail(Message message, Collection<String> emailAddresses) {
-        return false;
+        MimeMessage mail = emailSender.createMimeMessage();
+        if(emailAddresses != null && emailAddresses.size() > 0){
+            try {
+                MimeMessageHelper helper = new MimeMessageHelper(mail, false);
+                helper.setFrom(senderMailAddress);
+                String[] addrsArray = emailAddresses.toArray(new String[0]);
+                helper.setTo(addrsArray);
+                helper.setSubject(message.getSubject());
+                helper.setText(message.getContent());
+                this.emailSender.send(mail);
+            } catch (MessagingException e) {
+                e.printStackTrace();
+                return false;
+            }
+        }
+        return true;
     }
 }
--- apiMail/src/main/java/com/esgi/apimail/web/configs/BeanConfigs.java
+++ apiMail/src/main/java/com/esgi/apimail/web/configs/BeanConfigs.java
@@ -9,11 +9,23 @@ import com.esgi.apimail.domain.newproject.contracts.INewProjectRepository;
 import com.esgi.apimail.domain.sendmail.MailService;
 import com.esgi.apimail.domain.sendmail.contracts.IMailRepository;
 import com.esgi.apimail.domain.sendmail.contracts.IMailSender;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
 
 @Configuration
 public class BeanConfigs {
+    @Value("${mail.address}")
+    private String senderMailAddress;
+
+    @Value("${mail.password}")
+    private String mailPassword;
+
+
     @Bean
     public NewProjectService newProjectService(INewProjectRepository repository){
         return new NewProjectService(repository);
@@ -32,5 +44,29 @@ public class BeanConfigs {
     @Bean
     public ListUsersService listUsersService(IListUsersRepository repository){
         return new ListUsersService(repository);
+    }
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+
+        //hotmail
+        //mailSender.setHost("smtp.live.com");
+        //mailSender.setPort(25);
+
+        //gmail
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername(senderMailAddress);
+        mailSender.setPassword(mailPassword);
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
     }
 }
